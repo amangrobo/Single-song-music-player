56ce3e2e4766b68841affc2db59bdc37aeb1b29e
Aman Jee <amangrobo@gmail.com>
2018-10-19 01:25:23 +0530

    First Commit

---
+++ app/src/main/java/com/grobo/seekbarcontrol/MainActivity.java
@@ -0,0 +1,87 @@
+package com.grobo.seekbarcontrol;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mPlayer;
+    AudioManager audioManager;
+
+    public void playClicked(View view){
+        mPlayer.start();
+    }
+    public void pauseClicked(View view){
+        mPlayer.pause();
+    }
+    public void stopClicked(View view){
+        mPlayer.stop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlayer = MediaPlayer.create(this, R.raw.song);
+
+        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+
+        SeekBar volumeControl = (SeekBar) findViewById(R.id.seekBar);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curVolume);
+
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        final SeekBar positionControl = (SeekBar) findViewById(R.id.seekBar2);
+        positionControl.setMax(mPlayer.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                positionControl.setProgress(mPlayer.getCurrentPosition());
+            }
+        },0, 1000);
+
+        positionControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    mPlayer.seekTo(i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+    }
+}
